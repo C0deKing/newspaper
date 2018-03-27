c9b80ce1dc357e48a5b61fd16dd7c2327f817973
matt <matt@oaesoftware.com>
2018-03-26 21:34:20 -0400

    ports

--- web/index.js
+++ web/index.js
@@ -9,4 +9,4 @@ app.use('/public', express.static('public'))
 app.get('*', (req, res) =>  res.sendFile(__dirname + '/views/index.html'))
 
 
-app.listen(port, () => console.log('Example app listening at http://localhost:3000'))
+app.listen(port, () => console.log(`Example app listening at Port ${port}`))
