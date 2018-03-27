48c0676800e869976572699968bd5c012916d82d
matt <matt@oaesoftware.com>
2018-03-26 21:24:15 -0400

    prod changes

--- web/index.js
+++ web/index.js
@@ -1,3 +1,4 @@
+const port = process.env.PORT || 3000;
 const express = require('express')
 const app = express()
 
@@ -8,4 +9,4 @@ app.use('/public', express.static('public'))
 app.get('*', (req, res) =>  res.sendFile(__dirname + '/views/index.html'))
 
 
-app.listen(3000, () => console.log('Example app listening at http://localhost:3000'))
+app.listen(port, () => console.log('Example app listening at http://localhost:3000'))
