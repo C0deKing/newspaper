b8411cfb72571f9b6845b506c10692620261c645
matt <matt@oaesoftware.com>
2018-04-10 12:41:04 -0400

    adding last sql files needed

---
+++ mysql/procs/ap_getMyArticles.sql
@@ -0,0 +1,36 @@
+DELIMITER //
+drop PROCEDURE if exists ap_getMyArticles //
+CREATE PROCEDURE ap_getMyArticles(in _userId int, in _pageNumber int, 
+						in _pagesize int)
+BEGIN
+
+	declare _offset int default 0; 
+
+	set _offset := (_pageNumber - 1) * _pagesize; 
+
+	if fn_isUserEditor(_userId) then 
+		select SQL_CALC_FOUND_ROWS
+			id, 
+			headline, 
+			body,
+			isApproved,
+			publishDate,
+			createdAt,
+			updatedAt
+		from article
+		where userId = _userId
+		limit _pagesize offset _offset;
+
+
+		select found_rows();
+	else 
+		select "Only Editors and Admins can view articles" as 'message', 
+				-1 as 'errorCode'; 
+
+	end if; 
+
+	
+
+ 	
+END //
+DELIMITER ;
---
+++ mysql/procs/ap_getUsers.sql
@@ -0,0 +1,32 @@
+DELIMITER //
+drop PROCEDURE if exists ap_getUsers //
+CREATE PROCEDURE ap_getUsers(in _userId int, in _pageNumber int, 
+						in _pagesize int)
+BEGIN
+
+	declare _offset int default 0; 
+
+	set _offset := (_pageNumber - 1) * _pagesize; 
+
+	if fn_isUserAdmin(_userId) then 
+		select SQL_CALC_FOUND_ROWS
+			id, 
+			username, 
+			firstName, 
+			lastName
+		from st_user
+		limit _pagesize offset _offset;
+
+
+		select found_rows();
+	else 
+		select "Only Admins can view other users" as 'message', 
+				-1 as 'errorCode'; 
+
+	end if; 
+
+	
+
+ 	
+END //
+DELIMITER ;
