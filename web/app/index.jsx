413bf16c4d583387f21168a118d8335908ff4262
matt <matt@oaesoftware.com>
2018-04-14 13:52:28 -0400

    test change

--- web/app/index.jsx
+++ web/app/index.jsx
@@ -3,7 +3,7 @@ import {render} from 'react-dom';
 
 class App extends React.Component {
   render () {
-    return <p> Hello React, Test change  !</p>;
+    return <p> Hello React, Test change (again)  !</p>;
   }
 }
 
