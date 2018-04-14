38fade23bf96a7e832ca2ef2c0e2de04077057fd
matt <matt@oaesoftware.com>
2018-04-14 13:34:26 -0400

    adding react

---
+++ web/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+module.exports = {
+  entry: __dirname + '/app/index.jsx',
+  output: {
+    path: __dirname + '/public/build',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }, {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  }
+}
