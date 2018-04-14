270255cc6d80a97b79d383c619fc211b827b637e
matt <matt@oaesoftware.com>
2018-04-14 13:56:14 -0400

    set up for react dev

--- web/app/index.jsx
+++ web/app/index.jsx
@@ -3,7 +3,7 @@ import {render} from 'react-dom';
 
 class App extends React.Component {
   render () {
-    return <p> Hello React, Test change (again)  !</p>;
+    return <p> Hello Newspaper!</p>;
   }
 }
 
