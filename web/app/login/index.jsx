01728b324799fe71c53aa3a553d31fe1e5f0991e
matt <matt@oaesoftware.com>
2018-04-14 16:51:23 -0400

    adding login page

--- web/app/login/index.jsx
+++ web/app/login/index.jsx
@@ -12,7 +12,27 @@ class Login extends React.Component {
 	
 	render() {
 		return(
-			<div>Hello Login</div>
+			<div className="row justify-content-center" style={{paddingTop: "150px"}}>
+				<div className="col-4">
+					<div className="text-center mb-4">
+				        <h1 className="h3 mb-3 font-weight-normal">Sign In</h1>
+				      </div>
+
+				      <div className="form-label-group">
+				        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" />
+				        <label >Email address</label>
+				      </div>
+
+				      <div className="form-label-group">
+				        <input type="password" id="inputPassword" className="form-control" placeholder="Password"  />
+				        <label >Password</label>
+				      </div>
+
+				     
+				      <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+				      <p className="mt-5 mb-3 text-muted text-center">&copy; 2017-2018</p>
+				</div>
+			</div>
 	    )
 	}
 }
