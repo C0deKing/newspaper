1e75138606d779d4d75b2183e7d6ed34b4c653a7
matt <matt@oaesoftware.com>
2018-04-14 16:39:03 -0400

    adding routing

---
+++ web/app/login/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Login extends React.Component {
+	constructor(props){
+		super(props)
+	    this.state = {
+	      	
+	    }
+	}
+	componentDidMount() {
+	}
+	
+	render() {
+		return(
+			<div>Hello Login</div>
+	    )
+	}
+}
+
+
+
+export default Login
--- web/app/structure/body.jsx
+++ web/app/structure/body.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Published from '../published/index.jsx'
-
+import Login from '../login/index.jsx'
 class Body extends React.Component {
 	constructor(props){
 		super(props)
@@ -14,10 +14,13 @@ class Body extends React.Component {
 	
 	render() {
 		return(
+			<div className="row container">
 			<Switch>
 		      <Route exact path='/' component={Published}/>
 		      <Route path='/published' component={Published}/>
+		      <Route path='/login' component={Login}/>
 		    </Switch>
+		    </div>
 	    )
 	}
 }
