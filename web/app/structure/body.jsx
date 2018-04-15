59c1437e40c19a0f6df5664790ca2802190e40c9
matt <matt@oaesoftware.com>
2018-04-15 14:15:45 -0400

    login tweaks

--- web/app/structure/body.jsx
+++ web/app/structure/body.jsx
@@ -143,7 +143,7 @@ class Body extends React.Component {
 			<div>
 				<div className="row">
 					<nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{width:"100%", paddingLeft: "40px"}}>
-					  <Link to="/" className="navbar-brand" href="">Newspaper</Link>
+					  <Link to={this.state.authorized ? "/user" : "/"} className="navbar-brand" href="">Newspaper</Link>
 					  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
 					    <span className="navbar-toggler-icon"></span>
 					  </button>
